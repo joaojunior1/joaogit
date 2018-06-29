9f2defe16a611f6d429c1794dff709ae5a85ce3f
Alunos <Alunos@SL304PC09>
2018-06-29 20:16:09 -0300

    atualização classe

--- src/joaogit/Joaogit.java
+++ src/joaogit/Joaogit.java
@@ -3,9 +3,25 @@ package joaogit;
 
 
 public class Joaogit {
+        private String name ;
+        private String Frase;
 
-    public static void main(String[] args) {
-        
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrase() {
+        return Frase;
+    }
+
+    public void setFrase(String Frase) {
+        this.Frase = Frase;
     }
     
 }
